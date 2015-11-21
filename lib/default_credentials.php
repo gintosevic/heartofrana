d58ba656874b9e59254717d3fa912b2bd41fcbd0
gint <gint_@hotmail.com>
2015-11-21 23:52:55 +0100

    Removed useless dir (from a former version)

--- lib/default_credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_PORT", "3306");
-define("DB_NAME", "TO_BE_DEFINED");
-define("DB_USER", "TO_BE_DEFINED");
-define("DB_PASSWORD", "TO_BE_DEFINED");
-
-?>
